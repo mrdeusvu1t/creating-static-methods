b9039a76e993fd5162b4bded72cbc3e79e190e9f
Yury Hryshanau <mr.deusvult@gmail.com>
2020-11-12 15:36:03 +0300

    Up

--- CreatingMethods/MethodsWithRefParameters.cs
+++ CreatingMethods/MethodsWithRefParameters.cs
@@ -9,7 +9,6 @@
             return value;
         }
 
-        // TODO #17. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets "charValue" parameter ("char" type) with ref keyword. The method should return value of "charValue" parameter and set the "charValue" parameter to default value (use "default" operator).
         public static char ReturnParameterValueAndSetParameterToDefaultValue(ref char charValue)
         {
             char value = charValue;
@@ -17,7 +16,6 @@
             return value;
         }
 
-        // TODO #18. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets "floatValue" parameter ("float" type) with ref keyword. The method should return value of "floatValue" parameter and set the "floatValue" parameter to default value (use "default" operator).
         public static float ReturnParameterValueAndSetParameterToDefaultValue(ref float floatValue)
         {
             float value = floatValue;
@@ -25,7 +23,6 @@
             return value;
         }
 
-        // TODO #19. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets "intValue" parameter ("int" type) with ref keyword. The method should return value of "intValue" parameter and set the "intValue" parameter to default value (use "default" operator).
         public static int ReturnParameterValueAndSetParameterToDefaultValue(ref int intValue)
         {
             int value = intValue;
@@ -33,7 +30,6 @@
             return value;
         }
 
-        // TODO #20. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets "longValue" parameter ("long" type) with ref keyword. The method should return value of "longValue" parameter and set the "longValue" parameter to default value (use "default" operator).
         public static long ReturnParameterValueAndSetParameterToDefaultValue(ref long longValue)
         {
             long value = longValue;
--- CreatingMethods/PassthroughMethods.cs
+++ CreatingMethods/PassthroughMethods.cs
@@ -1,6 +1,5 @@
 ﻿namespace CreatingMethods
 {
-    // ReSharper disable once IdentifierTypo
     public static class PassthroughMethods
     {
         public static bool ReturnValue(bool boolValue)
@@ -8,73 +7,61 @@
             return boolValue;
         }
 
-        // TODO #1: Add a static method here with name "ReturnValue" that gets "byteValue" parameter ("byte" type) and returns it.
         public static byte ReturnValue(byte byteValue)
         {
             return byteValue;
         }
 
-        // TODO #2: Add a static method here with name "ReturnValue" that gets "sbyteValue" parameter ("sbyte" type) and returns it.
         public static sbyte ReturnValue(sbyte sbyteValue)
         {
             return sbyteValue;
         }
 
-        // TODO #3: Add a static method here with name "ReturnValue" that gets "charValue" parameter ("char" type) and returns it.
         public static char ReturnValue(char charValue)
         {
             return charValue;
         }
 
-        // TODO #4: Add a static method here with name "ReturnValue" that gets "decimalValue" parameter ("decimal" type) and returns it.
         public static decimal ReturnValue(decimal decimalValue)
         {
             return decimalValue;
         }
 
-        // TODO #5: Add a static method here with name "ReturnValue" that gets "doubleValue" parameter ("double" type) and returns it.
         public static double ReturnValue(double doubleValue)
         {
             return doubleValue;
         }
 
-        // TODO #6: Add a static method here with name "ReturnValue" that gets "floatValue" parameter ("float" type) and returns it.
         public static float ReturnValue(float floatValue)
         {
             return floatValue;
         }
 
-        // TODO #7: Add a static method here with name "ReturnValue" that gets "intValue" parameter ("int" type) and returns it.
         public static int ReturnValue(int intValue)
         {
             return intValue;
         }
 
-        // TODO #8: Add a static method here with name "ReturnValue" that gets "uintValue" parameter ("uint" type) and returns it.
         public static uint ReturnValue(uint uintValue)
         {
             return uintValue;
         }
 
-        // TODO #9: Add a static method here with name "ReturnValue" that gets "longValue" parameter ("long" type) and returns it.
         public static long ReturnValue(long longValue)
         {
             return longValue;
         }
 
-        // TODO #10: Add a static method here with name "ReturnValue" that gets "ulongValue" parameter ("ulong" type) and returns it.
         public static ulong ReturnValue(ulong ulongValue)
         {
             return ulongValue;
         }
 
-        // TODO #11: Add a static method here with name "ReturnValue" that gets "shortValue" parameter ("short" type) and returns it.
         public static short ReturnValue(short shortValue)
         {
             return shortValue;
         }
 
-        // TODO #12: Add a static method here with name "ReturnValue" that gets "ushortValue" parameter ("ushort" type) and returns it.
         public static ushort ReturnValue(ushort ushortValue)
         {
             return ushortValue;
